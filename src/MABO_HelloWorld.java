c952832ced44dae5dec46a3d2354fb5bb835d15f
mabolivar <ma.bolivar643@gmail.com>
2014-10-02 11:59:39 -0500

    Improve notation

--- src/MABO_HelloWorld.java
+++ src/MABO_HelloWorld.java
@@ -1,25 +1,29 @@
 
 public class MABO_HelloWorld {
-	
-	
+
+
 	/**
 	 * This is Manuel's project
 	 */
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo: Método para calcular exponentes");
-		System.out.println("2^4 = " + exp(2, 4));
+		int b =2;
+		int ex = 25;
+		System.out.println(b+"^"+ex+" = " + expo(b, ex));
 	}
-	
-	
-	public static int exp(int base , int exponente){
+
+
+	public static int expo(int base , int exponente){
 		int ans = 1;
-		
+
 		while (exponente>0) {
 			ans = ans * base;
 			exponente--;
 		}
 		return ans;
 	}
-	
+
+
+
 }
